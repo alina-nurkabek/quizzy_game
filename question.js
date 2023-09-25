cf949f801b398a3161fd175718618fda35ea4a3c
Alina Nurkabekova <119506615+alina-nurkabek@users.noreply.github.com>
2023-09-25 15:56:20 +0600

    Add files via upload

---
+++ question.js
@@ -0,0 +1,115 @@
+//creating an array and passing the number
+
+let questions=[
+    {
+        numb:1,
+        question:" In the Josephus problem, if there are 10 people standing in a circle and every 3rd person is eliminated, what is the position of the last remaining person??",
+        answer:"4",
+        options:[
+            "5",
+            "4",
+            "7",
+            "6"
+        ]
+    },
+    {
+        numb:2,
+        question:"In the Tower of Hanoi puzzle, what is the minimum number of moves required to solve a 4-disk problem?",
+        answer:"15",
+        options:[
+            "17",
+            "15",
+            "13",
+            "12"
+        ]
+    },
+    {
+        numb:3,
+        question:"Which mathematical technique is used to approximate solutions to problems by adding small perturbations to known solutions?",
+        answer:"Perturbation Method",
+        options:[
+            "Josephus Method",
+            "Tower of Hanoi Method",
+            "Perturbation Method",
+            "Floor & Ceiling"
+        ]
+    },
+    {
+        numb:4,
+        question:"In the Josephus problem, if there are 15 people standing in a circle and every 4th person is eliminated, what is the position of the last remaining person?",
+        answer:"3",
+        options:[
+            "3",
+            "4",
+            "23",
+            "6"
+        ]
+    },
+    {
+        numb:5,
+        question:"If x = 5.7, what is the floor and ceiling of x?",
+        answer:"5,6",
+        options:[
+            "4,5",
+            "5,6",
+            "7,8",
+            "9,11"
+        ]
+    },
+    {
+        numb:6,
+        question:"Euler's Theorem: State Euler's theorem and explain its significance in number theory.",
+        answer:"Euler's theorem states that a^φ(n) ≡ 1 (mod n) for any integer a and positive integer n.",
+        options:[
+            "Euler's theorem states that a^φ(n) ≡ 1 (mod n) for any integer a and positive integer n.",
+            "Euler's theorem states that a^φ(n) ≡ 0 (mod n) for any integer a and positive integer n.",
+            "Euler's theorem states that a^φ(n) ≡ a (mod n) for any integer a and positive integer n.",
+            "Euler's theorem states that a^φ(n) ≡ -a (mod n) for any integer a and positive integer n."
+        ]
+    },
+    {
+        numb:7,
+        question:"Given four positive integers a, b, c, and d, prove that GCD(a, b) * GCD(c, d) = GCD(AC, BD), where AC denotes the product of a and c, and BD denotes the product of b and d.",
+        answer:"GCD(a, b) * GCD(c, d) = GCD(AC, BD)",
+        options:[
+            "GCD(a, c) * GCD(b, d) = GCD(AC, BD)",
+            "GCD(a, b) * GCD(c, d) = GCD(AC, BD)",
+            "GCD(a, b) * GCD(c, d) = GCD(A + B, C + D)",
+            "GCD(a, c) * GCD(b, d) = GCD(A + B, C + D)"
+        ]
+    },
+    {
+        numb:8,
+        question:" Prove that the sum of any three consecutive positive odd integers is always divisible by 3.",
+        answer:"True",
+        options:[
+            "False",
+            "Only for the first three odd integers",
+            "True",
+            "Only for the last three odd integers"
+        ]
+    },
+    {
+        numb:9,
+        question:"Given two positive integers a and b, prove that GCD(a, b) * LCM(a, b) = a * b.",
+        answer:"GCD(a, b) * LCM(a, b) = a * b",
+        options:[
+            "GCD(a, b) * GCD(c, d) = GCD(A + B, C + D)",
+            "GCD(a, b) * LCM(a, b) = a * b",
+            "GCD(a, c) * GCD(b, d) = GCD(AC, BD)",
+            "GCD(a, c) * GCD(b, d) = GCD(A + B, C + D)"
+        ]
+    },
+    {
+        numb:10,
+        question:"Josephus Method: In the Josephus problem with 30 people sitting in a circle, every 7th person is eliminated. Determine the position of the last survivor standing.",
+        answer:"16",
+        options:[
+            "8",
+            "12",
+            "16",
+            "18"
+        ]
+    }
+
+]
